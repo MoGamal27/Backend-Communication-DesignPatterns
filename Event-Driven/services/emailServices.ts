13bf8054ad05e511f4499b9ccfb7aa02f9952433
MoGamal27 <113600854+MoGamal27@users.noreply.github.com>
2024-07-25 19:07:15 +0300

    create email service to send email

---
+++ Event-Driven/services/emailServices.ts
@@ -0,0 +1,29 @@
+import nodemailer from 'nodemailer'
+import dotenv from 'dotenv'
+import emailConfig from '../config/email';
+dotenv.config();
+
+interface UserData{
+    name: string;
+    email: string;
+}
+
+const sendEmail = (userData: UserData): void => {
+
+        const transporter = nodemailer.createTransport({
+            service: 'Gmail',
+            auth: {
+              user: process.env.EMAIL_USER,
+              pass: process.env.EMAIL_PASS
+            }
+          });
+
+     transporter.sendMail({
+        from: 'Event-Driven <J5lqz@example.com>',
+        to: userData.email,
+        subject: 'Welcome',
+        text: `Welcome ${userData.name}!, Thank you for registering with us.`,
+    })
+}
+
+export default sendEmail
